893d2ca4268cd5513baef931f4686a86e5d7ad80
jlmayo <jlmayo22@gmail.com>
2022-10-25 21:08:32 -0700

    Edited table schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE departments (
-  id INT NOT NULL,
+  id INT PRIMARY KEY,
   dept_name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-  id INT,
+  id INT PRIMARY KEY,
   job_title VARCHAR(30),
   salary DECIMAL,
   dept_id INT,
@@ -20,7 +20,7 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employees (
-  id INT,
+  id INT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
